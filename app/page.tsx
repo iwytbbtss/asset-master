d7b8da4719ba543a5edbd4b5514245ef915989c6
우재윤 <104556563+iwytbbtss@users.noreply.github.com>
2024-05-10 09:55:08 +0900

    chore: install shadcn@ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
 import Image from 'next/image';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 const Home = () => {
   return (
@@ -8,6 +16,18 @@ const Home = () => {
           Get started by editing&nbsp;
           <code className='font-mono font-bold'>app/page.tsx</code>
         </p>
+        <Dialog>
+          <DialogTrigger>Open</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your account and remove
+                your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
         <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none'>
           <a
             className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
